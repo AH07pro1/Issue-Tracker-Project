80404596a87d0436b5e501580744c296f0a81e78
AH07pro1 <adam.hilali@gmail.com>
2025-03-09 12:35:30 -0400

    page prerendering issue

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,15 +1,24 @@
 'use client';
 import { useSession } from 'next-auth/react';
-import React from 'react'
+import React from 'react';
 import ProfileTemplate from './profileTemplate';
 
 const Page = () => {
-    const {data: session } = useSession();
-  return (
-    <div>
-      <ProfileTemplate name={session!.user.name!} email={session!.user.email!} profilePic={session!.user.image!}/>
-    </div>
-  )
-}
+    const { data: session } = useSession();
 
-export default Page
+    if (!session || !session.user) {
+        return <div>Loading...</div>; // Or a proper "Not logged in" UI
+    }
+
+    return (
+        <div>
+            <ProfileTemplate
+                name={session.user.name ?? 'Guest'}
+                email={session.user.email ?? 'No email available'}
+                profilePic={session.user.image ?? '/default-profile.png'}
+            />
+        </div>
+    );
+};
+
+export default Page;

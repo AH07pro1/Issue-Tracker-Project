1ab0b8afa91923109862684a5d6d580d9b190559
AH07pro1 <adam.hilali@gmail.com>
2025-03-09 11:12:31 -0400

    update next-auth config

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,38 +1,5 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import GithubProvider from "next-auth/providers/github";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
+import { authOptions } from "@/app/lib/auth"
+import NextAuth from "next-auth"
 
-// Initialize Prisma client
-const prisma = new PrismaClient();
-
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.AUTH_GOOGLE_ID!,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
-    }),
-    GithubProvider({
-      clientId: process.env.AUTH_GITHUB_ID!,
-      clientSecret: process.env.AUTH_GITHUB_SECRET!,
-    }),
-  ],
-  callbacks: {
-    // Type the session and user parameters here using the custom type from next-auth.d.ts
-    async session({ session, user }) {
-      if (user) {
-        session.user.id = user.id;
-        session.user.role = ((user as unknown) as { role: string }).role || "user"; // Ensure role exists
-      }
-      return session;
-    },
-  },
-  session: {
-    strategy: "database",
-  },
-};
-
-export const GET = NextAuth(authOptions);
-export const POST = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
---
+++ app/lib/auth.ts
@@ -0,0 +1,35 @@
+import{ AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+// Initialize Prisma client
+const prisma = new PrismaClient();
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+    GithubProvider({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+  ],
+  callbacks: {
+    // Type the session and user parameters here using the custom type from next-auth.d.ts
+    async session({ session, user }) {
+      if (user) {
+        session.user.id = user.id;
+        session.user.role = ((user as unknown) as { role: string }).role || "user"; // Ensure role exists
+      }
+      return session;
+    },
+  },
+  session: {
+    strategy: "database",
+  },
+};
